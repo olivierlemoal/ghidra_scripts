43ce278cd143c9df0b2f71f8bb84c5f66b071629
Olivier Le Moal <mail@olivierlemoal.fr>
2020-03-13 11:13:29 +0100

    more verbose

--- MiniDebugInfo.py
+++ MiniDebugInfo.py
@@ -3,14 +3,20 @@
 #@category FunctionID
 #@menupath Tools.Function ID.MiniDebugInfo
 
-from ghidra.program.model.symbol import SourceType
 import subprocess
+import sys
+import os.path
+from ghidra.program.model.symbol import SourceType
 
-path = currentProgram.executablePath
+path_binary = currentProgram.executablePath
 functionManager = currentProgram.getFunctionManager()
 addressFactory = currentProgram.getAddressFactory()
 
-process = subprocess.Popen(['eu-readelf', '-Ws', '--elf-section', path], stdout=subprocess.PIPE)
+if not os.path.isfile(path_binary):
+    print("Can't find file : {}".format(path_binary))
+    sys.exit(1)
+
+process = subprocess.Popen(['eu-readelf', '-Ws', '--elf-section', path_binary], stdout=subprocess.PIPE)
 stdout = process.communicate()[0].splitlines()
 
 for l in stdout[4:]:
@@ -20,6 +26,7 @@ for l in stdout[4:]:
         func_name = fields[7]
         f = functionManager.getFunctionAt(address)
         if f:
+            print("Function {} found at {}".format(func_name, address))
             f.setName(func_name, SourceType.USER_DEFINED)
         else:
             print("No function found at {} ({})".format(address, func_name))
